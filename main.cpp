87fd707bfdf9551d2ea8b67a1be6a044ee1fdb24
TIGames <kisstp2006@gmail.com>
2026-07-18 00:23:09 +0200

    small cleanups and now there is 10 cubes in the scene

--- main.cpp
+++ main.cpp
@@ -14,12 +14,10 @@
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
 
-#include <iostream>
-#include <cstdio>
+
+
 #include <string>
-#include <cmath>
 #include <memory>
 #include <utility>
 
@@ -72,6 +70,19 @@ namespace
     -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
     -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
+
+    glm::vec3 cubePositions[] = {
+    glm::vec3( 0.0f,  0.0f,  0.0f),
+    glm::vec3( 2.0f,  5.0f, -15.0f),
+    glm::vec3(-1.5f, -2.2f, -2.5f),
+    glm::vec3(-3.8f, -2.0f, -12.3f),
+    glm::vec3( 2.4f, -0.4f, -3.5f),
+    glm::vec3(-1.7f,  3.0f, -7.5f),
+    glm::vec3( 1.3f, -2.0f, -2.5f),
+    glm::vec3( 1.5f,  2.0f, -2.5f),
+    glm::vec3( 1.5f,  0.2f, -1.5f),
+    glm::vec3(-1.3f,  1.0f, -1.5f)
+};
 
     class SandboxApplication final : public Application
     {
@@ -202,9 +213,34 @@ namespace
                 texture2_->bind(1);
 
                 shader_->use();
-
                 glBindVertexArray(vao_);
-                glDrawArrays(GL_TRIANGLES, 0, 36);
+
+                for (unsigned int i = 0; i < 10; ++i)
+                {
+                    glm::mat4 model = glm::mat4(1.0f);
+
+                    model = glm::translate(
+                        model,
+                        cubePositions[i]
+                    );
+
+                    const float angle = 20.0f * static_cast<float>(i);
+
+                    model = glm::rotate(
+                        model,
+                        glm::radians(angle),
+                        glm::vec3(1.0f, 0.3f, 0.5f)
+                    );
+
+                    shader_->setMat4("model", model);
+
+                    glDrawArrays(
+                        GL_TRIANGLES,
+                        0,
+                        36
+                    );
+                }
+
                 glBindVertexArray(0);
             }
 
